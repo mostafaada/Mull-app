55e0aafe2b820120222cbfcd6d08aabd26d94bc1
mostafaada <132032534+mostafaada@users.noreply.github.com>
2023-04-28 05:05:12 +0200

    upload backend for mull-app

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { AuthModule } from './auth/auth.module';
+import { TrashModule } from './trash/trash.module';
+import { MitarbeiterModule } from './mitarbeiter/mitarbeiter.module';
+import { AdminModule } from './admin/admin.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [AuthModule, TrashModule, MitarbeiterModule, AdminModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/trash/trash.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Post, Body, Patch, Param, Get } from '@nestjs/common';
+import { TrashService } from './trash.service';
+
+@Controller('trash')
+export class TrashController {
+  constructor(private service: TrashService) {}
+
+  @Get('')
+  async viewAllTrashes() {
+    const result = await this.service.findAll();
+    return result;
+  }
+
+  @Get(':id')
+  async viewOneTrash(@Param('id') id) {
+    const result = await this.service.findOne(id);
+    return result;
+  }
+
+  @Post()
+  async createTrash(@Body() body: any) {
+    const result = await this.service.addOne(body);
+    return result;
+  }
+
+  @Patch(':userId/:id')
+  async updateTrash(
+    @Param('userId') userId,
+    @Param('id') id,
+    @Body() body: any,
+  ) {
+    const result = await this.service.updateOne(userId, id, body);
+    return result;
+  }
+}
